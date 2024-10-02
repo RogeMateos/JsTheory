c6c703ef488c73d8e4ecf867555bfc2a5fa20e38
Roge <rogemateos@hotmail.com>
2024-10-02 16:36:11 +0100

    add links file

---
+++ 01 Links.js
@@ -0,0 +1,160 @@
+//Links
+01 js Definitions
+
+function add(param1, param2) { // write code here. return param1+ param2; }
+
+7 CONTEPTS https://dev.to/nathannosudo/every-developer-must-know-these-7-javascript-concepts-3ecp
+
+ARRAYS https://dev.to/brojenuel/javascript-array-tips-and-tricks-you-should-know-3d0n?utm_source=digest_mailer&utm_medium=email&utm_campaign=digest_email
+
+ARROW FUNCTIONS https://dmitripavlutin.com/differences-between-arrow-and-regular-functions/
+
+https://ed.team/blog/arrow-functions-en-javascript?fbclid=IwAR14xmGVx3PKgoMQLQ2x6fWy7t8E1htP6bE95NrzmBq5-GD27Q3i78nqBQ4
+
+https://zellwk.com/blog/arrow-function-best-practices/?ck_subscriber_id=142525981
+
+Arrow functions and closures https://vmarchesin.medium.com/javascript-arrow-functions-and-closures-4e53aa30b774
+
+ARRAY METHOD https://dev.to/doabledanny/javascript-array-method-cheat-sheet-17-must-know-methods-441g
+
+https://dev.to/thatanjan/top-10-array-methods-to-learn-to-become-a-pro-in-javascript-1ic9
+
+ASYNCHRONIMOUS
+
+https://lenguajejs.com/javascript/asincronia/que-es/
+
+https://dev.to/joyshaheb/learn-callbacks-promises-async-await-by-making-ice-cream-4n76
+
+https://www.codigocorrecto.com/javascript/async-await-en-javascript-todo-lo-que-necesitas-saber/?fbclid=IwAR3qxc1Wg0zHA5EwIWD-d7-dXsLFAi03Zzuz5jmxRIz_lotKMJwbDRC43WM
+
+https://www.arquitecturajava.com/javascript-sincrono-o-asincrono/
+
+API que es https://www.youtube.com/watch?v=3nssV6l3gHg
+
+BIND https://academind.com/learn/javascript/function-bind-event-execution/
+
+No es mas que asignar nuevos contextos
+
+https://www.youtube.com/watch?v=AjQsp-RpQ6o
+
+BLUEWEB Fundamentos https://bluuweb.github.io/javascript/fundamentos/#var-vs-let-vs-const
+
+CALLBACKS https://www.geeksforgeeks.org/javascript-callbacks/ Dentro de una función el callback llama a otra función
+
+Callbacks,promises,Asynch https://dev.to/nas5w/asynchronous-javascript-how-callbacks-promises-and-async-await-work-1f7p?utm_source=digest_mailer&utm_medium=email&utm_campaign=digest_email
+
+https://dev.to/joyshaheb/learn-callbacks-promises-async-await-by-making-ice-cream-4n76
+
+https://shadidhaque.medium.com/javascript-callbacks-vs-promise-vs-observable-in-plain-english-b678548f333c
+
+CALLBACKS HELL CHEAT SHEET https://dev.to/sm0ke/javascript-basics-and-cheat-sheet-o39
+
+CALLBACKS HELL https://codearmy.co/que-es-el-callback-hell-y-como-evitarlo-4af418a6ed14
+
+CLASSLIST https://www.w3schools.com/jsref/prop_element_classlist.asp
+
+CLOSURES https://dev.to/shimphillip/javascript-closure-simply-explained-1f79
+
+https://dev.to/reubengt/a-simple-example-i-use-to-remember-what-a-closure-is-1ap2
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+
+https://dev.to/papercoding22/how-to-explain-javascript-closure-to-a-5-years-old-kid-4d6n?utm_source=digest_mailer&utm_medium=email&utm_campaign=digest_email
+
+CONSOLE.LOG AND RETURN https://www.quora.com/What-is-the-difference-between-console-log-and-return-in-JavaScript?share=1
+
+COHERSION https://medium.freecodecamp.org/js-type-coercion-explained-27ba3d9a2839
+
+Cohersion Table https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness
+
+Destructuring an Object https://wesbos.com/destructuring-objects
+
+https://github.com/wesbos/wesbos/blob/master/src/posts/A%20Dead%20Simple%20intro%20to%20Destructuring%20JavaScript%20Objects/A%20Dead%20Simple%20intro%20to%20Destructuring%20JavaScript%20Objects.mdx
+
+https://www.freecodecamp.org/news/destructuring-patterns-javascript-arrays-and-objects/?fbclid=IwAR2FaOx-Gu3iNU4ErTmOiGKBQTYzS0WGuTIMJViqgE4x0QScr4gtecS8YhA
+
+EXECUTION CONTEXT
+
+https://dev.to/kpnigalye/how-javascript-really-works-1p0m?utm_source=digest_mailer&utm_medium=email&utm_campaign=digest_email
+
+ENCAPSULATION https://www.intertech.com/Blog/encapsulation-in-javascript/
+
+Event Delegation https://www.youtube.com/watch?v=yQRiBGeygeA
+
+https://javascript.info/event-delegation
+
+https://medium.com/@bretdoucette/part-4-what-is-event-delegation-in-javascript-f5c8c0de2983
+
+https://www.youtube.com/watch?v=bOd2HIDh7cU
+
+EVENT TARGET Target https://www.w3schools.com/jsref/tryit.asp?filename=try_dom_event_target
+
+FUNCTIONS https://medium.com/@aresnik11/a-guide-to-function-declarations-function-expressions-and-arrow-functions-in-javascript-f19c05a7c25
+
+https://dmitripavlutin.com/6-ways-to-declare-javascript-functions/
+
+https://dev.to/victordeandres/funciones-en-javascript-7-formas-de-declarar-una-funcion-523a
+
+Hoisting https://medium.com/the-boujoukos-bulletin/javascript-scope-hoisting-and-arrow-functions-8f80684be458
+
+https://escuela.it/cursos/curso-avanzado-javascript
+
+Incrementing/Decrementing ++ — https://codeburst.io/javascript-increment-and-decrement-8c223858d5ed
+
+IF/ELSE STAMENTS https://medium.freecodecamp.org/javascript-essentials-how-to-make-life-decisions-with-if-else-statements-1908ff7cf5da
+
+INTERVIEW https://dev.to/codewithfahad/my-web-dev-interview-experience-and-questions-i-was-asked-5cg9?utm_source=digest_mailer&utm_medium=email&utm_campaign=digest_email
+
+https://dev.to/worldindev/400-javascript-interview-questions-with-answers-2fcj
+
+https://medium.com/javascript-scene/10-interview-questions-every-javascript-developer-should-know-in-2024-c1044bcb0dfb
+
+LET CONST
+
+https://bluuweb.github.io/javascript/fundamentos/#var-vs-let-vs-const
+
+Maps https://www.linkedin.com/pulse/introduction-maps-javascript-all-you-need-know-alex-devero/?trackingId=OrRyIdBxwpWN%2FlziIhWHmQ%3D%3D
+
+NULL-UNDEFINED https://code-boxx.com/javascript-null-vs-undefined-vs-empty/
+
+OPERATOR mano + = https://www.codeleaks.io/javascript-addition-assignment-operator/
+
+PROYECTOS GIT HUB https://github.com/florinpop17/app-ideas
+
+Oriented Programming https://www.freecodecamp.org/news/object-oriented-programming-concepts-21bb035f7260/
+
+PROMISES
+
+https://www.itdo.com/blog/javascript-que-es-una-promise/
+
+https://dev.to/spartakyste/the-promises-guide-i-would-have-loved-as-a-junior-developper-3621
+
+https://dev.to/reubengt/promises-promise-all-and-async-await-explained-in-5-minutes-2dld
+
+https://www.freecodecamp.org/news/learn-promise-async-await-in-20-minutes/
+
+https://www.neoguias.com/promesas-javascript/
+
+REACT CONCEPTS
+
+https://dev.to/jps27cse/learn-this-before-react-4hpl
+
+RETURN AND CONSOLE LOG https://www.quora.com/What-is-the-difference-between-console-log-and-return-in-JavaScript?share=1
+
+SET TIMEOUT IN LOOP https://coderwall.com/p/_ppzrw/be-careful-with-settimeout-in-loops
+
+https://www.youtube.com/watch?v=5_yOH6tkPZI
+
+Hace todo el loop y llega a 5 pero después de el segundo añade uno mas , entonces es seis
+
+SPREAD OPERATOR https://www.programiz.com/javascript/spread-operator
+
+SYNCHRONIMOS https://dev.to/makoto0825/asyncsync-in-javascript-438e?context=digest
+
+TERNARY OPERATOR https://dev.to/commdao/thinking-about-ternary-operators-1167
+
+THIS https://dev.to/adii/javascripts-this-keyword-understanding-its-power-and-usage-j70
+
+Var/let/const https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/
+
+Valor y referencia https://ed.team/cursos/javascript/02/03
